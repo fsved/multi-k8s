5865d4118dcb04fea7b1253db50f07b1b3294ab8
LFS <fredrik.svedhem@finaps.nl>
2020-07-23 14:05:03 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t fsved/std-complex_client:latest -t fsved/std-complex_client:$SHA -f ./client/Dockerfile ./client
+docker build -t fsved/std-complex_server:latest -t fsved/std-complex_server:$SHA -f ./server/Dockerfile ./server
+docker build -t fsved/std-complex_worker:latest -t fsved/std-complex_worker:$SHA -f ./worker/Dockerfile ./worker
+docker push fsved/std-complex_client:latest
+docker push fsved/std-complex_server:latest
+docker push fsved/std-complex_worker:latest
+
+docker push fsved/std-complex_client:$SHA
+docker push fsved/std-complex_server:$SHA
+docker push fsved/std-complex_worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=fsved/std-complex_server:$SHA
+kubectl set image deployments/client-deployment client=fsved/std-complex_client:$SHA
+kubectl set image deployments/worker-deployment worker=fsved/std-complex_worker:$SHA
